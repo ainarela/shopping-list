1e7ab654acb405b9356e6c841c250908b90ec855
Aina Requena <aina.requena@gmail.com>
2015-02-13 18:10:33 +0100

    Applied hide/show items functionality

--- js/app.js
+++ js/app.js
@@ -11,8 +11,22 @@ $(document).ready(function(){
 	// Hide/Show completed items
 	$('.hide-show__button').click(function(event){
 		event.preventDefault();
-		$('.list__item').has('.list__item-checkbox:checked').toggle();
+		var h = $(this).hasClass('completed-items-are-shown');
+		var s = $(this).hasClass('completed-items-are-hidden');
+		if (h == true) {
+			$(this).removeClass('completed-items-are-shown').addClass('completed-items-are-hidden').html('Show completed items');
+			$('.list__item').has('.list__item-checkbox:checked').addClass('hide');
+		} else if (s == true) {
+			$(this).removeClass('completed-items-are-hidden').addClass('completed-items-are-shown').html('Hide completed items');
+			$('.list__item').has('.list__item-checkbox:checked').removeClass('hide');
+		}
 	});
-
+	$('.list__item-checkbox').click(function(){
+		var s = $('.hide-show__button').hasClass('completed-items-are-hidden');
+		if ( s == true){
+			// $('.list__item').has('.list__item-checkbox:checked').not('hide').addClass('hide');
+			$('.list__item').has('.list__item-checkbox:checked').addClass('hide');
+		};
+	});	
 	 
 });
