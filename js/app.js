2121697ab6a3aa536ac688e7cf016fefff9851e8
Aina Requena <aina.requena@gmail.com>
2015-02-23 17:57:47 +0100

    Code Refactoring

--- js/app.js
+++ js/app.js
@@ -1,31 +1,54 @@
 $(document).ready(function(){
 	// Add a new item to the list
-	$('.new-item').submit(function(event){
-		var text_value = $('.new-item__text').val();
+	$('.new-item').submit(addNewItem);
+
+	// Hide-Show completed items
+	$('.hide-show__button').click(hideShow);
+
+	// Check-Uncheck items
+	$('.list').on('click', '.list__item-checkbox', checkItems);
+
+	// Delete a particular item
+	$('.list').on('click', '.delete-item-button', function deleteItem(){
+		$('.list__item').has(this).remove();
+	});
+
+	// Functions
+
+	function addNewItem(event){
 		event.preventDefault();
+		var htmlStr = '', 
+			text_value = $('.new-item__text').val();
 		if (text_value != '') {
-			$('.list').prepend('<li class="list__item"><input type="checkbox" class="list__item-checkbox"><label class="list__item-name">'+text_value+'</label><button class="delete-item-button">&#xf217;</button></li>');
-			$('.new-item__text').val('');
+			htmlStr += '<li class="list__item"><input type="checkbox" class="list__item-checkbox"><label class="list__item-name">';
+			htmlStr += text_value;
+			htmlStr += '</label><button class="delete-item-button">&#xf217;</button></li>'
 		}
-	});
-	// Hide/Show completed items
-	$('.hide-show__button').click(function(event){
+		$('.list').prepend(htmlStr);
+		$('.new-item__text').val('');
+	};
+
+	function isHidden(that){
+		var hidden = $(that).hasClass('completed-items-are-hidden');
+		return hidden;
+	};
+
+	function hideShow(event){
 		event.preventDefault();
-		var shown = $(this).hasClass('completed-items-are-shown');
-		var hidden = $(this).hasClass('completed-items-are-hidden');
-		if (shown == true) {
-			$(this).removeClass('completed-items-are-shown').addClass('completed-items-are-hidden').html('Show completed items');
-			$('.list__item').has('.list__item-checkbox:checked').addClass('hide');
-		} else if (hidden == true) {
-			$(this).removeClass('completed-items-are-hidden').addClass('completed-items-are-shown').html('Hide completed items');
+		var hidden = isHidden(this);
+		if (hidden == true) {
+			$(this).removeClass('completed-items-are-hidden').html('Hide completed items');
 			$('.list__item').has('.list__item-checkbox:checked').removeClass('hide');
+		} else {
+			$(this).addClass('completed-items-are-hidden').html('Show completed items');
+			$('.list__item').has('.list__item-checkbox:checked').addClass('hide');
 		}
-	});
-	$('.list').on('click', '.list__item-checkbox', function(){
-		var hidden = $('.hide-show__button').hasClass('completed-items-are-hidden');
+	}
+
+	function checkItems(){
+		var hidden = isHidden($('.hide-show__button'));
 		var checked = $(this).is(':checked');
 		if  (hidden == true && checked == true){
-			// $('.list__item').has('.list__item-checkbox:checked').not('hide').addClass('hide');
 			$('.list__item').has(this).addClass('hide');
 		} 
 		if (checked == true){
@@ -35,10 +58,6 @@ $(document).ready(function(){
 			// If the item is unchecked, it is placed at the beginning of the list 
 			$('.list__item').has(this).prependTo('.list');
 		}
-	});	
-	// Delete a particular item
-	$('.list').on('click', '.delete-item-button', function(){
-		$('.list__item').has(this).remove();
-	});
+	}
 	 
 });
